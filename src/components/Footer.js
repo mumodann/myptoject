4c1a423ecea312fefa762c79fde77a804219b36c
mumodann <92576480+mumodann@users.noreply.github.com>
2022-11-28 17:59:34 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from 'react'
 const footer = () => (
   <footer className='footer'>
     <div className='footer__copyright'>
-      &copy; 2021 Made with ❤️ by{' '}
-      <a className='footer__copyright--link' href='https://github.com/AndresXI'>
+      &copy; 2022 Made with ❤️ by{' '}
+      <a className='footer__copyright--link' href='https://github.com/mumodann'>
         {' '}
-        Andres Alcocer
+        Mumo Daniel
       </a>
     </div>
   </footer>
